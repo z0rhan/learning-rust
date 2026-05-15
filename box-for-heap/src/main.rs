0bc8a7aa2a11f8627865bacb47a668b4175d8306
z0rhan <uknowme1928@gmail.com>
2026-05-16 01:36:47 +0300

    15.1 & 15.2: Box<T> and deref for smart-pointers

---
+++ box-for-heap/src/main.rs
@@ -0,0 +1,94 @@
+use List::{Cons, Nil};
+use std::ops::Deref;
+use std::ops::DerefMut;
+
+// From &T to &U when T: Deref<Target = U>
+// From &mut T to &mut U when T: DerefMut<Target = U>
+// From &mut T to &U when T: Deref<Target = U>
+// immutable ref cannot be coerced to mutable ref
+
+fn main() {
+    box_for_heap();
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(x , 5);
+    // Here, we can deref y because we implemented the Deref trait for MyBox
+    // BTS *y = *(y.deref())
+    assert_eq!(*y, 5);
+
+    // Deref coercion
+    // So basically conversion of ref to a type that implements Deref to
+    // ref of another type
+    hello("Rust");
+    // Deref coercion makes it possible to make the function_call below
+    // So, str is coerced to &str by str.deref() -> &String and .deref() again
+    // to get &str
+    let str = Box::new("Rust");
+    hello(&str);
+    // Without deref coercion, it would be,
+    hello(&(*str)[..]);
+
+    // Likewise for mutable dereferencing, we have DerefMut trait
+    let mut z = MyBox::new(5);
+    println!("z : {}", *z);
+    *z = 6;
+    println!("z : {}", *z);
+}
+
+fn hello(str: &str) {
+    println!("Hello, {str}")
+}
+
+fn box_for_heap() {
+    let x = 5;
+    // y is now a ref to x
+    let y = &x;
+    // Similary, z is now a ref but to a copied value of x on the heap
+    let z = Box::new(x);
+
+    assert_eq!(x, 5);
+    // Here, we have to deref y to get to value of x (5)
+    assert_eq!(*y, 5);
+    // Here, we have to deref z to get to its value of 5 on the heap
+    assert_eq!(*z, 5);
+
+    // Box allows to store data on the heap rather than on stack
+    // We can use them like any other variables on the stack
+    let b = Box::new(8);
+    println!("b = {b}");
+
+    // Helps with defining recursive data structure such as cons below
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
+enum List {
+    // With just List, the size is not known at compile time
+    // Box helps with that
+    Cons(i32, Box<List>),
+    Nil
+}
+
+// Tuple struct
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
